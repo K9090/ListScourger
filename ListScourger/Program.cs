e3ef7178878ee1a271300abcfe57fd4d658af4fc
Timo K9090 <k9090@student.jamk.fi>
2018-04-27 01:08:32 +0300

    Initial commit

---
+++ ListScourger/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListScourger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> almostIntactList = new List<int>();
+
+            int j = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                almostIntactList.Add(i);
+            }
+
+            almostIntactList.RemoveAt(rnd.Next(0, 100));
+
+            Console.Write("| ");
+            foreach (int item in almostIntactList)
+            {
+                Console.Write(item + " | ");
+            }
+            Console.WriteLine();
+
+            foreach (int item in almostIntactList)
+            {
+                j++;
+                if (item != j)
+                {
+                    Console.WriteLine("The missing link: " + (item - 1));
+                    j++;
+                }
+            }
+        }
+    }
+}
